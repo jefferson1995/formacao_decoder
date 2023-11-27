51aca9a440f73db2496a39eee2b870007adaf179
Jefferson Barbosa <jefferson.bds@hotmail.com>
2023-11-27 07:51:52 -0300

    Create: UserCOntroller

---
+++ backend/authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+
+}
