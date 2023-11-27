724a6b7fbe50db3321ae8a9ad074caebd91023c5
Jefferson Barbosa <jefferson.bds@hotmail.com>
2023-11-27 08:23:42 -0300

    Create: Métodos para buscar todos usuários e usuário por id

--- backend/authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ backend/authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -13,6 +18,21 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não existe.");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
+        }
+    }
 
 
 }
--- backend/authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ backend/authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,14 +1,34 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @GetMapping
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
 
 }
