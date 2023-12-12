342e3db528ce6cdb28469498429a49db4afe72ab
Jefferson Barbosa <jefferson.bds@hotmail.com>
2023-12-12 08:59:09 -0300

    feat: adicionado hiperlink em getAllUsers

--- backend/authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ backend/authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -22,6 +22,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/users")
@@ -35,6 +38,11 @@ public class UserController {
                                                         @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC)
                                                        Pageable pageable) {
         Page<UserModel> userModelPage = userService.findAll(spec, pageable);
+        if(!userModelPage.isEmpty()){
+            for (UserModel user : userModelPage.toList()){
+                user.add(linkTo(methodOn(UserController.class).getOneUser(user.getUserId())).withSelfRel());
+            }
+        }
         return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
     }
 
