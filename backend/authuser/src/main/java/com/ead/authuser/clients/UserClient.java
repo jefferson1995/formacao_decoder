69e9baf97566216bd51fa1e2e12063a75be904ff
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-01-08 08:52:44 -0300

    feat: requisição de course com RestTemplate

--- backend/authuser/src/main/java/com/ead/authuser/clients/UserClient.java
+++ backend/authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -1,10 +1,48 @@
 package com.ead.authuser.clients;
 
+import com.ead.authuser.dtos.CourseDTO;
+import com.ead.authuser.dtos.ResponsePageDTO;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.UUID;
+
+@Log4j2
 @Component
 public class UserClient {
 
+    @Autowired
+    RestTemplate restTemplate;
 
+    String REQUEST_URI = "http://localhost:8082";
+
+    public Page<CourseDTO> getAllCoursesByUser(UUID userId, Pageable pageable) {
+        List<CourseDTO> searchResult = null;
+        String url = REQUEST_URI + "/courses?userId=" + userId + "&page=" + pageable.getPageNumber() + "&size=" +
+                pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
+        log.debug("Request URL: {}", url);
+        log.info("Request URL: {} ", url);
+        try {
+            ParameterizedTypeReference<ResponsePageDTO<CourseDTO>> responseType = new ParameterizedTypeReference<ResponsePageDTO<CourseDTO>>() {
+            };
+            ResponseEntity<ResponsePageDTO<CourseDTO>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
+            log.debug("Número de elementos retornados: {} ", searchResult.size());
+        } catch (HttpStatusCodeException e) {
+            log.error("Error Request /courses  ", e);
+        }
+        log.info("Request finalizada /courses userId {} ", userId);
+        return new PageImpl<>(searchResult);
+    }
 
 }
--- backend/authuser/src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ backend/authuser/src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -1,7 +1,9 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.clients.UserClient;
 import com.ead.authuser.dtos.CourseDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -20,11 +22,13 @@ import java.util.UUID;
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class UserCourseController {
 
-    @GetMapping("users/{userId}/courses")
-    public ResponseEntity<Page<CourseDTO>> getAllCoursesByUser(@PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
-                                                               @PathVariable(value = "userId") UUID userId) {
+    @Autowired
+    UserClient userClient;
 
-        return ResponseEntity.status(HttpStatus.OK).body();
+    @GetMapping("users/{userId}/courses")
+    public ResponseEntity<Page<CourseDTO>> getAllCoursesByUser(@PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
+                                                               @PathVariable(value = "userId") UUID userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(userClient.getAllCoursesByUser(userId, pageable));
 
     }
 
