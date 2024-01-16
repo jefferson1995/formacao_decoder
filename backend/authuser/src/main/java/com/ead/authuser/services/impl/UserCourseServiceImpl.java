f18747f5221155cbbb346829e11912181936c407
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-01-16 06:59:56 -0300

    feat: Criado método para salvar UserCourseModel

--- backend/authuser/src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
+++ backend/authuser/src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -1,11 +1,16 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserCourseRepository;
+import com.ead.authuser.services.UserCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
-public class UserCourseServiceImpl {
+public class UserCourseServiceImpl implements UserCourseService {
 
     final
     UserCourseRepository userCourseRepository;
@@ -16,4 +21,13 @@ public class UserCourseServiceImpl {
     }
 
 
+    @Override
+    public boolean existsByUserAndCourseId(UserModel userModel, UUID courseId) {
+        return userCourseRepository.existsByUserAndCourseId(userModel, courseId);
+    }
+
+    @Override
+    public UserCourseModel save(UserCourseModel userCourseModel) {
+        return userCourseRepository.save(userCourseModel);
+    }
 }
