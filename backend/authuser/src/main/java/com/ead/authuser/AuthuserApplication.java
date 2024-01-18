a8504117a0188a976790324fc5ff561be22ac4ba
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-01-18 07:37:20 -0300

    build: adicionado dependencias do eureka netflix

--- backend/authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
+++ backend/authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -2,6 +2,7 @@ package com.ead.authuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class AuthuserApplication {
