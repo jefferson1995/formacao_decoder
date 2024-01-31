def4892317ee1ffb029e9329fa9d9f9475a96c32
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-01-31 09:03:59 -0300

    feat: Mapear State Transfer Pattern com Replicação Parcial de Dados

--- backend-exemplo-comunicacao-assincrona/course/src/main/java/com/ead/course/controllers/CourseUserController.java
+++ backend-exemplo-comunicacao-assincrona/course/src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -4,6 +4,7 @@ import com.ead.course.dtos.SubscriptionDTO;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.services.UserService;
+import com.ead.course.specifications.SpecificationTemplate;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -26,23 +27,25 @@ public class CourseUserController {
     CourseService courseService;
 
     @Autowired
-    UserService courseUserService;
+    UserService userService;
 
     @GetMapping("/courses/{courseId}/users")
-    public ResponseEntity<Object> getAllUsersByCourse(@PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
+    public ResponseEntity<Object> getAllUsersByCourse(SpecificationTemplate.UserSpec spec,
+                                                      @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
                                                       @PathVariable(value = "courseId") UUID courseId) {
 
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
         if (!courseModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não existente. ");
         }
-        return ResponseEntity.status(HttpStatus.OK).body("");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.findAll(SpecificationTemplate.userCourseId(courseId).and(spec), pageable));
     }
 
     @PostMapping("/courses/{courseId}/users/subscription")
     public ResponseEntity<Object> saveSubscriptionUserInCourse(@PathVariable(value = "courseId") UUID courseId,
                                                                @RequestBody @Valid SubscriptionDTO subscriptionDTO) {
-        ResponseEntity<UserDTO> responseUser;
+        //ResponseEntity<UserDTO> responseUser;
 
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
         if (!courseModelOptional.isPresent()) {
--- backend-exemplo-comunicacao-assincrona/course/src/main/java/com/ead/course/services/impl/UserServiceImpl.java
+++ backend-exemplo-comunicacao-assincrona/course/src/main/java/com/ead/course/services/impl/UserServiceImpl.java
@@ -1,8 +1,12 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.UserModel;
 import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,4 +20,8 @@ public class UserServiceImpl implements UserService {
     }
 
 
+    @Override
+    public Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable) {
+        return userRepository.findAll(spec, pageable);
+    }
 }
