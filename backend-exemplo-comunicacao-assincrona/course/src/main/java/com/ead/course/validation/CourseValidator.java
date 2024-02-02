62c0891f04ea52bf9804b49fcbd28fe35f29f743
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-02-02 08:55:55 -0300

    fix: correção CourseValidator

--- backend-exemplo-comunicacao-assincrona/course/src/main/java/com/ead/course/validation/CourseValidator.java
+++ backend-exemplo-comunicacao-assincrona/course/src/main/java/com/ead/course/validation/CourseValidator.java
@@ -1,12 +1,17 @@
 package com.ead.course.validation;
 
 import com.ead.course.dtos.CourseDTO;
+import com.ead.course.enums.UserType;
+import com.ead.course.models.UserModel;
+import com.ead.course.services.UserService;
+import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
@@ -16,6 +21,9 @@ public class CourseValidator implements Validator {
     @Autowired
     @Qualifier("defaultValidator")
     private Validator validator;
+
+    @Autowired
+    UserService userService;
 
 
     @Override
@@ -33,19 +41,14 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors) {
-        //ResponseEntity<UserDTO> responseUserInstructor;
-        /*
-       try {
-           responseUserInstructor = authUserClient.getOneUserById(userInstructor);
-           if (responseUserInstructor.getBody().getUserType().equals(UserType.STUDENT)){
-               errors.rejectValue("userInstructor", "UserInstructorError", "Usuário precisa ser ADMIN ou INSTRUCTOR");
-           }
-       }catch (HttpStatusCodeException e){
-            if(e.getStatusCode().equals(HttpStatus.NOT_FOUND)){
-                errors.rejectValue("userInstructor", "UserInstructorError", "Instrutor/Admin não encontrado.");
-            }
-       }
-*/
+        Optional<UserModel> userModelOptional = userService.findById(userInstructor);
+        if (!userModelOptional.isPresent()) {
+            errors.rejectValue("userInstructor", "UserInstructorError", "Instrutor/Admin não encontrado.");
+        }
+        if (userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())) {
+            errors.rejectValue("userInstructor", "UserInstructorError", "Usuário dever ser Instrutor/Admin.");
+        }
+
     }
 
 
