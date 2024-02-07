c053f4ea5466cfa742c5709c4a04443cd6471992
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-02-07 09:05:02 -0300

    feat: Implementar Retry Resilience4j e Configurar Timeout

--- backend-exemplo-comunicacao-assincrona/authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ backend-exemplo-comunicacao-assincrona/authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,11 +3,13 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDTO;
 import com.ead.authuser.dtos.ResponsePageDTO;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -31,6 +34,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URI_COURSE;
 
+    @Retry(name = "retryInstance", fallbackMethod = "retryfallback")
     public Page<CourseDTO> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDTO> searchResult = null;
         ResponseEntity<ResponsePageDTO<CourseDTO>> result = null;
@@ -50,5 +54,10 @@ public class CourseClient {
         return result.getBody();
     }
 
+    public Page<CourseDTO> retryfallback(UUID userId, Pageable pageable, Throwable t) {
+        log.error("Acionado o  retryfallback, causa: {}", t.toString());
+        List<CourseDTO> searchResult = new ArrayList<>();
+        return new PageImpl<>(searchResult);
+    }
 
 }
