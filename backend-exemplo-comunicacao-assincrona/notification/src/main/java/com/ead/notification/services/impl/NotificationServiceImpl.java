f83ad5fa0a738a43011660dc07e81748009552b1
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-02-14 11:27:54 -0300

    feat: Preparação microservice notification - entidades, services e repositories

---
+++ backend-exemplo-comunicacao-assincrona/notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ead.notification.services.impl;
+
+import com.ead.notification.repositories.NotificationRepository;
+import com.ead.notification.services.NotificationService;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationServiceImpl implements NotificationService {
+
+    final
+    NotificationRepository notificationRepository;
+
+    public NotificationServiceImpl(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    
+}
