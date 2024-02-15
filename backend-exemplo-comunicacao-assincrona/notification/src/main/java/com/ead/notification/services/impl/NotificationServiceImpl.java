d8af5dd55818f98c8abe8b327b77614748278543
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-02-15 08:42:22 -0300

    feat: Criar Exchange Topic, definir Queue e Bindings

--- backend-exemplo-comunicacao-assincrona/notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
+++ backend-exemplo-comunicacao-assincrona/notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ead.notification.services.impl;
 
+import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
 import com.netflix.discovery.converters.Auto;
@@ -16,5 +17,9 @@ public class NotificationServiceImpl implements NotificationService {
         this.notificationRepository = notificationRepository;
     }
 
-    
+
+    @Override
+    public NotificationModel saveNotification(NotificationModel notificationModel) {
+        return notificationRepository.save(notificationModel);
+    }
 }
