6286c4f9abc8c350c7852dbf85705729926c98ae
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-03-06 10:15:06 -0300

    feat: Implementar Validation Tokens e Authorization com JWT

--- backend-exemplo-comunicacao-assincrona/authuser/src/main/java/com/ead/authuser/configs/security/JwtProvider.java
+++ backend-exemplo-comunicacao-assincrona/authuser/src/main/java/com/ead/authuser/configs/security/JwtProvider.java
@@ -1,7 +1,6 @@
 package com.ead.authuser.configs.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -28,5 +27,26 @@ public class JwtProvider {
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
     }
+
+    public String getUsernameJwt(String token){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validadeJwt(String authToken){
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        }catch (SignatureException e){
+            log.error("Invalid JWT signature: {}", e.getMessage());
+        }catch (MalformedJwtException e){
+            log.error("Invalid JWT token: {}", e.getMessage());
+        }catch (ExpiredJwtException e){
+            log.error("JWT está expirado: {}", e.getMessage());
+        }catch (IllegalArgumentException e){
+            log.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
 
 }
