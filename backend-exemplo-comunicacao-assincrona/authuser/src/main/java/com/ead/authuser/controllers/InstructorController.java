611c40c9c97c363420ef60e28e21fef4c7aede5b
Jefferson Barbosa <jefferson.bds@hotmail.com>
2024-03-20 09:09:15 -0300

    feat: Authorization Roles para Instructor

--- backend-exemplo-comunicacao-assincrona/authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ backend-exemplo-comunicacao-assincrona/authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -2,14 +2,18 @@ package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.InstructorDTO;
 import com.ead.authuser.dtos.UserDTO;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,15 +31,22 @@ public class InstructorController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RoleService roleService;
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDTO instructorDTO) {
         Optional<UserModel> userModelOptional = userService.findById(instructorDTO.getUserId());
         if (!userModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não existe.");
         } else {
+            RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR)
+                    .orElseThrow(() -> new RuntimeException("Error: Perfil não encontrado."));
             var userModel = userModelOptional.get();
             userModel.setUserType(UserType.INSTRUCTOR);
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userModel.getRoles().add(roleModel);
             userService.updateUser(userModel);
             return ResponseEntity.status(HttpStatus.OK).body(userModel);
         }
--- backend-exemplo-comunicacao-assincrona/course/src/main/java/com/ead/course/validation/CourseValidator.java
+++ backend-exemplo-comunicacao-assincrona/course/src/main/java/com/ead/course/validation/CourseValidator.java
@@ -1,12 +1,15 @@
 package com.ead.course.validation;
 
+import com.ead.course.configs.security.AuthenticationCurrentUserService;
 import com.ead.course.dtos.CourseDTO;
 import com.ead.course.enums.UserType;
 import com.ead.course.models.UserModel;
 import com.ead.course.services.UserService;
 import com.netflix.discovery.converters.Auto;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -14,7 +17,7 @@ import org.springframework.validation.Validator;
 import java.util.Optional;
 import java.util.UUID;
 
-
+@Log4j2
 @Component
 public class CourseValidator implements Validator {
 
@@ -24,6 +27,9 @@ public class CourseValidator implements Validator {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    AuthenticationCurrentUserService authenticationCurrentUserService;
 
 
     @Override
@@ -41,15 +47,21 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors) {
-        Optional<UserModel> userModelOptional = userService.findById(userInstructor);
-        if (!userModelOptional.isPresent()) {
-            errors.rejectValue("userInstructor", "UserInstructorError", "Instrutor/Admin não encontrado.");
-        }
-        if (userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())) {
-            errors.rejectValue("userInstructor", "UserInstructorError", "Usuário dever ser Instrutor/Admin.");
-        }
+        UUID currentUserId = authenticationCurrentUserService.getCurrentUser().getUserId();
+        if (currentUserId.equals(userInstructor)) {
+            Optional<UserModel> userModelOptional = userService.findById(userInstructor);
+            if (!userModelOptional.isPresent()) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "Instrutor/Admin não encontrado.");
+            }
+            if (userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "Usuário dever ser Instrutor/Admin.");
+            }
 
+        } else {
+            log.warn("user instructor: {}", userInstructor);
+            log.warn("user autenticado: {}", currentUserId);
+            throw new AccessDeniedException("Forbidden");
+        }
     }
-
 
 }
